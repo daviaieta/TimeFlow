ee6ccfe2ff9aca81f996dfa71945028210c43dc3
DaviAieta <aieta.davi@gmail.com>
2026-07-08 20:21:33 -0300

    chore: implementing error handler & helper of hashing password

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
 import "dotenv/config";
-import { fastify } from "fastify";
+import { fastify, FastifyError, FastifyReply, FastifyRequest } from "fastify";
+import { AppError } from "./lib/errors";
 
 const app = fastify();
 
+app.setErrorHandler((error: FastifyError, request: FastifyRequest, reply: FastifyReply) => {
+  if (error instanceof AppError) {
+    return reply.status(error.statusCode).send({ message: error.message });
+  }
+
+  if (error.validation) {
+    return reply.status(400).send({ message: error.message });
+  }
+
+  request.log.error(error);
+  return reply.status(500).send({ message: "Internal server error" });
+});
+
 app.get("/", async () => {
-  return { message: "API" };
+  return { message: "Welcome to BOOKING SAAS" };
 });
 
 // REGISTER ROUTES HERE
